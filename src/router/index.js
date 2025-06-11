9ef22e3bbda8d999f2ac1adb9799e64d053f4487
Akangbe Ooreoluwa <akangbepattie@gmail.com>
2025-06-11 23:17:27 +0100

    added fonts and color variables

---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+import { createBrowserRouter } from "react-router-dom";
+
+import Home from "@/pages/home";
+import ErrorPage from "@/components/common/error-page";
+import SingleTodo from "@/pages/single-todo";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Home,
+    ErrorBoundary: ErrorPage,
+  },
+  {
+    path: "todo/:id",
+    Component: SingleTodo,
+    ErrorBoundary: ErrorPage,
+  },
+]);
+
+export { router };
